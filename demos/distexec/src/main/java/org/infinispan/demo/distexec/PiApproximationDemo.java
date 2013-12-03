ebdc1996d962505e17af6be377e0ac3fd1aac169
akilman <akilman@appdynamics.com>
2013-12-03 12:08:50 -0800

    Update canonical PiApproximationDemo to continually compute Pi

--- demos/distexec/src/main/java/org/infinispan/demo/distexec/PiApproximationDemo.java
+++ demos/distexec/src/main/java/org/infinispan/demo/distexec/PiApproximationDemo.java
@@ -15,98 +15,102 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.LockSupport;
 
 /**
  * Infinispan distributed executors demo using pi approximation.
  */
 public class PiApproximationDemo extends Demo {
-   private static final int DEFAULT_NUM_POINTS = 50000000;
-   private int numPoints;
+    private static final int DEFAULT_NUM_POINTS = 50000000;
+    private int numPoints;
 
-   public static void main(String... args) throws Exception {
-      new PiApproximationDemo(args).run();
-   }
+    public static void main(String... args) throws Exception {
+        new PiApproximationDemo(args).run();
+    }
 
-   public PiApproximationDemo(String[] args) throws Exception {
-      super(args);
-      numPoints = commandLineOptions.getInt("numPoints");
-   }
+    public PiApproximationDemo(String[] args) throws Exception {
+        super(args);
+        numPoints = commandLineOptions.getInt("numPoints");
+    }
 
-   public void run() throws Exception {
-      // Step 1: start cache.
-      Cache<String, String> cache = startCache();
+    public void run() throws Exception {
+        // Step 1: start cache.
+        Cache<String, String> cache = startCache();
 
-      // Step 2: run Pi Approximation
-      try {
-         if (isMaster) {
-            int numServers = cache.getCacheManager().getMembers().size();
-            int numberPerWorker = numPoints / numServers;
+        // Step 2: run Pi Approximation
+        try {
+            if (isMaster) {
+                while (true)
+                {
+                    int numServers = cache.getCacheManager().getMembers().size();
+                    int numberPerWorker = numPoints / numServers;
 
-            DistributedExecutorService des = new DefaultExecutorService(cache);
+                    DistributedExecutorService des = new DefaultExecutorService(cache);
 
-            long start = System.currentTimeMillis();
-            List<Future<Integer>> results = des.submitEverywhere(new CircleTest(numberPerWorker));
+                    long start = System.currentTimeMillis();
+                    List<Future<Integer>> results = des.submitEverywhere(new CircleTest(numberPerWorker));
 
+                    int insideCircleCount = 0;
+
+                    for (Future<Integer> f : results) insideCircleCount += f.get(10, TimeUnit.MINUTES);
+
+                    double appxPi = 4.0 * insideCircleCount / numPoints;
+
+                    System.out.printf("Pi approximation is %s, computed in %s using %s nodes.%n", appxPi, Util.prettyPrintTime(System.currentTimeMillis() - start), numServers);
+                }
+
+            } else {
+                System.out.println("Slave node waiting for Map/Reduce tasks.  Ctrl-C to exit.");
+                LockSupport.park();
+            }
+        } finally {
+            cache.getCacheManager().stop();
+        }
+    }
+
+    private static class CircleTest implements Callable<Integer>, Serializable {
+
+        /**
+         * The serialVersionUID
+         */
+        private static final long serialVersionUID = 3496135215525904755L;
+
+        private final int loopCount;
+
+        public CircleTest(int loopCount) {
+            this.loopCount = loopCount;
+        }
+
+        @Override
+        public Integer call() throws Exception {
             int insideCircleCount = 0;
+            for (int i = 0; i < loopCount; i++) {
+                double x = Math.random();
+                double y = Math.random();
+                if (insideCircle(x, y))
+                    insideCircleCount++;
+            }
+            return insideCircleCount;
+        }
 
-            for (Future<Integer> f : results) insideCircleCount += f.get();
+        private boolean insideCircle(double x, double y) {
+            return (Math.pow(x - 0.5, 2) + Math.pow(y - 0.5, 2)) <= Math.pow(0.5, 2);
+        }
+    }
 
-            double appxPi = 4.0 * insideCircleCount / numPoints;
-
-            System.out.printf("Pi approximation is %s, computed in %s using %s nodes.%n", appxPi, Util.prettyPrintTime(System.currentTimeMillis() - start), numServers);
-
-         } else {
-            System.out.println("Slave node waiting for Map/Reduce tasks.  Ctrl-C to exit.");
-            LockSupport.park();
-         }
-      } finally {
-         cache.getCacheManager().stop();
-      }
-   }
-
-   private static class CircleTest implements Callable<Integer>, Serializable {
-
-      /**
-       * The serialVersionUID
-       */
-      private static final long serialVersionUID = 3496135215525904755L;
-
-      private final int loopCount;
-
-      public CircleTest(int loopCount) {
-         this.loopCount = loopCount;
-      }
-
-      @Override
-      public Integer call() throws Exception {
-         int insideCircleCount = 0;
-         for (int i = 0; i < loopCount; i++) {
-            double x = Math.random();
-            double y = Math.random();
-            if (insideCircle(x, y))
-               insideCircleCount++;
-         }
-         return insideCircleCount;
-      }
-
-      private boolean insideCircle(double x, double y) {
-         return (Math.pow(x - 0.5, 2) + Math.pow(y - 0.5, 2)) <= Math.pow(0.5, 2);
-      }
-   }
-
-   @Override
-   protected SimpleJSAP buildCommandLineOptions() throws JSAPException {
-      return new SimpleJSAP("PiApproximationDemo",
-                            "Approximate Pi using Infinispan DistributedExecutorService ", new Parameter[]{
-                  new FlaggedOption("configFile", JSAP.STRING_PARSER, "config-samples/distributed-udp.xml",
-                                    JSAP.NOT_REQUIRED, 'c', "configFile",
-                                    "Infinispan config file"),
-                  new FlaggedOption("nodeType", JSAP.STRING_PARSER, "slave", JSAP.REQUIRED,
-                                    't', "nodeType", "Node type as either master or slave"),
-                  new FlaggedOption("numPoints", JSAP.INTEGER_PARSER,
-                                    String.valueOf(DEFAULT_NUM_POINTS), JSAP.REQUIRED, 'n',
-                                    "numPoints", "Total number of darts to shoot"),
-            });
-   }
+    @Override
+    protected SimpleJSAP buildCommandLineOptions() throws JSAPException {
+        return new SimpleJSAP("PiApproximationDemo",
+                "Approximate Pi using Infinispan DistributedExecutorService ", new Parameter[]{
+                new FlaggedOption("configFile", JSAP.STRING_PARSER, "config-samples/distributed-udp.xml",
+                        JSAP.NOT_REQUIRED, 'c', "configFile",
+                        "Infinispan config file"),
+                new FlaggedOption("nodeType", JSAP.STRING_PARSER, "slave", JSAP.REQUIRED,
+                        't', "nodeType", "Node type as either master or slave"),
+                new FlaggedOption("numPoints", JSAP.INTEGER_PARSER,
+                        String.valueOf(DEFAULT_NUM_POINTS), JSAP.REQUIRED, 'n',
+                        "numPoints", "Total number of darts to shoot"),
+        });
+    }
 }
